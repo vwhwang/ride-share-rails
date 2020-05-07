f23c4296a50645531ea02c9adcf09a584aaea490
Rosalyn <syrosalynyu@gmail.com>
2020-05-07 13:45:07 -0700

    Created actions and strong params in drivers_controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,33 +1,26 @@
-DRIVERS = [
-  {
-  id: 1,
-  name: "Bernardo Prosacco",
-  vin: "WBWSS52P9NEYLVDE9",
-  available: true,
-  },
-  {
-    id: 2,
-    name: "Emory Rosenbaum",
-    vin: "1B9WEX2R92R12900E",
-    available: true,
-  },
-
-]
-
 class DriversController < ApplicationController
   def index
-    @drivers = DRIVERS
+    @drivers = Driver.all
   end
 
   def new
+    @driver = Driver.new
   end
 
   def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to driver_path(@driver.id)
+      return
+    else
+      render :new
+      return
+    end  
   end
 
   def show
-    driver_id = params[:id].to_i
-    @driver = DRIVERS[driver_id]
+    @driver = Driver.find_by(id: params[:id])
 
     if @driver.nil?
       head :not_found
@@ -36,12 +29,48 @@ class DriversController < ApplicationController
   end
 
   def edit
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
   end
 
   def update
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found
+      return
+    elsif @driver.update(driver_params)
+      redirect_to driver_path(@driver.id)
+      return
+    else
+      render :edit
+      return
+    end  
   end
 
   def destroy
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found
+      return
+    else
+      @driver.destroy
+      redirect_to drivers_path
+      return
+    end
   end
+
+  # TODO change_status & patch route.
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :available)
+  end 
 
 end
