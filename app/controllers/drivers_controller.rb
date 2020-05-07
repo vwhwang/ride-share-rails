03229efa0de0b373106bb193d32f6a37b4ad417c
Rosalyn <syrosalynyu@gmail.com>
2020-05-06 19:01:11 -0700

    1.) Completed index and show action in the controller 2.) Draft the drivers index view and show view

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,47 @@
+DRIVERS = [
+  {
+  id: 1,
+  name: "Bernardo Prosacco",
+  vin: "WBWSS52P9NEYLVDE9",
+  available: true,
+  },
+  {
+    id: 2,
+    name: "Emory Rosenbaum",
+    vin: "1B9WEX2R92R12900E",
+    available: true,
+  },
+
+]
+
 class DriversController < ApplicationController
+  def index
+    @drivers = DRIVERS
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    driver_id = params[:id].to_i
+    @driver = DRIVERS[driver_id]
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
