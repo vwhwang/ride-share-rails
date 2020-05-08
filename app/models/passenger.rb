f60859e7321e561a67c80bf7013d64b58f038952
vwhwang <vwhwang38@gmail.com>
2020-05-08 13:42:11 -0700

    added total charged in passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,12 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_charged
+    total = 0 
+    self.trips.each do |trip|
+      total += trip.cost 
+    end  
+    return "$#{ '%.2f' % total}"
+  end 
+  
 end
