e8dce4531ef997cf0989a32973313f61d99075e0
Rosalyn <syrosalynyu@gmail.com>
2020-05-09 20:21:31 -0700

    Created a ,ethod in Driver model to calculate the total earning of a driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -15,4 +15,15 @@ class Driver < ApplicationRecord
     return (total_rating / count).round(2)
   end
 
+  def total_earning
+    total = 0
+    count = 0
+    self.trips.each do |trip|
+      total += trip.cost
+      count += 1
+    end
+    result = ((total - (1.65 * count)) * 0.8).round(2)
+    return result
+  end
+
 end
