60df8d19fb546318c8b3e2d31989f9a176d85ab3
Rosalyn <syrosalynyu@gmail.com>
2020-05-10 22:57:08 -0700

    Added trip controller, routes, and views

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,47 @@
+class TripsController < ApplicationController
+  def show
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
+      return
+    # elsif @trip.update(driver_params)
+      redirect_to trip_path(@trip.id)
+      return
+    else
+      render :edit
+      return
+    end  
+  end
+
+  def destroy
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
+      return
+    else
+      @trip.destroy
+      redirect_to passengers_path
+      return
+    end
+  end
+end
